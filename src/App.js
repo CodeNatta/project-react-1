b93643609d8915ac41e88c62fd0e9ff91288d07a
CodeNatta <98188523+CodeNatta@users.noreply.github.com>
2022-10-13 20:18:18 +0530

    react router done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import './App.css'
 import Alert from './Components/Alert';
-// import About from './Components/About';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
   const [mode, setMode]=useState('light');
@@ -23,7 +24,11 @@ function App() {
     if(mode==='dark'){
         setMode('light');
         document.body.style.backgroundColor='white';
-        showAlert("Light Mode activated","success")
+        showAlert("Light Mode activated","success");
+        document.title='Light Mode'
+        setInterval(()=>{ document.title='Install now'
+      },2000)
+        
     }
     else{
     setMode('dark');
@@ -33,11 +38,16 @@ function App() {
 }
   return (
     <>
+    <Router>
       <Navbar title= "Character Checker" aboutUs="About us" blog="Blogs" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
-        <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter your text Here to check words" mode={mode}/>
-        </div>
+      <div className="container my-3">
+        <Routes>
+          <Route path="/about" element={<About/>}/>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter your text Here to check words" mode={mode}/>}></Route>
+        </Routes>
+      </div>
+      </Router>
       </>
   );
 }
